436743129850d090a7f23251709c82dece047fd9
heesane <tees3359@gmail.com>
2024-05-31 01:59:38 +0900

    feat: Location Repository

--- backend/src/main/java/pocket/backend/location/domain/LocationRepository.java
+++ backend/src/main/java/pocket/backend/location/domain/LocationRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface LocationRepository extends JpaRepository<Location,Long>{
+public interface LocationRepository extends JpaRepository<Location,Long> {
     Boolean existsByName(String locationName);
     Optional<Location> findByName(String locationName);
 }
