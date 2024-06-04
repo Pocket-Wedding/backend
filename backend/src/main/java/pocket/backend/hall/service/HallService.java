3c4ae2389247e7607077b3a857cce0dc6c0161ea
limjihoon99 <bo9701@naver.com>
2024-06-04 09:03:19 +0900

    feat: Hall Service 작성

--- backend/src/main/java/pocket/backend/hall/service/HallService.java
+++ backend/src/main/java/pocket/backend/hall/service/HallService.java
@@ -1,4 +1,36 @@
 package pocket.backend.hall.service;
 
+import org.springframework.stereotype.Service;
+import pocket.backend.hall.domain.Hall;
+import pocket.backend.hall.domain.HallRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class HallService {
+
+    private final HallRepository hallRepository;
+
+    public HallService(HallRepository hallRepository) {
+        this.hallRepository = hallRepository;
+    }
+
+    public Hall saveHall(Hall hall) {
+        return hallRepository.save(hall);
+    }
+
+    public List<Hall> getAllHalls() {
+        return hallRepository.findAll();
+    }
+
+    public Optional<Hall> getHallById(Long id) {
+        return hallRepository.findById(id);
+    }
+
+    public void deleteHall(Long id) {
+        hallRepository.deleteById(id);
+    }
+
+    
 }
