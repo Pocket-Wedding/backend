af0dd7885e020a34c7ca44a854721dabcd9fd695
limjihoon99 <bo9701@naver.com>
2024-06-06 14:48:54 +0900

    refactor: 불필요한 import 삭제

--- backend/src/main/java/pocket/backend/hall/domain/HallRepository.java
+++ backend/src/main/java/pocket/backend/hall/domain/HallRepository.java
@@ -2,7 +2,6 @@ package pocket.backend.hall.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import pocket.backend.hall.domain.Hall;
 
 @Repository
 public interface HallRepository extends JpaRepository<Hall, Long> {
