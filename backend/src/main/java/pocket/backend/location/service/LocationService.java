ff5565e82efd6823278f46a59fce900ffa6414f9
heesane <tees3359@gmail.com>
2024-06-08 04:02:30 +0900

    feat: getLocationByName

--- backend/src/main/java/pocket/backend/location/service/LocationService.java
+++ backend/src/main/java/pocket/backend/location/service/LocationService.java
@@ -3,6 +3,7 @@ package pocket.backend.location.service;
 import jakarta.transaction.Transactional;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pocket.backend.common.exceptions.DuplicatedException;
 import pocket.backend.common.exceptions.ErrorCode;
@@ -11,11 +12,13 @@ import pocket.backend.location.domain.Location;
 import pocket.backend.location.domain.LocationRepository;
 import pocket.backend.location.dto.LocationRequest;
 
+import java.util.Optional;
+
 @RequiredArgsConstructor(access= AccessLevel.PUBLIC)
 @Service
 public class LocationService{
 
-    private LocationRepository locationRepository;
+    private final LocationRepository locationRepository;
 
     // 해당 위치의 업체가 몇개인지 확인하는 메서드
     public Long countLocation(String name) {
@@ -25,12 +28,17 @@ public class LocationService{
 
         return findLocation.getTotalCount();
     }
+
     public Long getLocationIdByName(String name) {
         Location findLocation = locationRepository.findByName(name).orElseThrow(
                 () -> new NotFoundException(ErrorCode.NOT_FOUND_LOCATION)
         );
 
         return findLocation.getId();
+    }
+
+    public Optional<Location> getLocationByName(String name) {
+        return locationRepository.findByName(name);
     }
     // 해당 위치를 등록하는 메서드
     @Transactional
