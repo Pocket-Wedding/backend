c9756b4da3b8e1bb042e8d326f1cf4e1a360d5ba
heesane <tees3359@gmail.com>
2024-06-08 03:28:35 +0900

    feat: Location Service update & delete 추가

--- backend/src/main/java/pocket/backend/location/service/LocationService.java
+++ backend/src/main/java/pocket/backend/location/service/LocationService.java
@@ -1,19 +1,20 @@
 package pocket.backend.location.service;
 
+import jakarta.transaction.Transactional;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pocket.backend.common.exceptions.DuplicatedException;
 import pocket.backend.common.exceptions.ErrorCode;
 import pocket.backend.common.exceptions.NotFoundException;
 import pocket.backend.location.domain.Location;
 import pocket.backend.location.domain.LocationRepository;
+import pocket.backend.location.dto.LocationRequest;
 
 @RequiredArgsConstructor(access= AccessLevel.PUBLIC)
 @Service
 public class LocationService{
-    @Autowired
+
     private LocationRepository locationRepository;
 
     // 해당 위치의 업체가 몇개인지 확인하는 메서드
@@ -24,16 +25,41 @@ public class LocationService{
 
         return findLocation.getTotalCount();
     }
+    public Long getLocationIdByName(String name) {
+        Location findLocation = locationRepository.findByName(name).orElseThrow(
+                () -> new NotFoundException(ErrorCode.NOT_FOUND_LOCATION)
+        );
 
+        return findLocation.getId();
+    }
     // 해당 위치를 등록하는 메서드
-    public void registerLocation(String name) {
-        if(locationRepository.existsByName(name)) {
+    @Transactional
+    public void registerLocation(LocationRequest locationRequest) {
+        if(locationRepository.existsByName(locationRequest.getName())) {
             throw new DuplicatedException(ErrorCode.DUPLICATED_LOCATION_NAME);
         }
         Location location = Location.builder()
-                .name(name)
+                .name(locationRequest.getName())
                 .build();
 
         locationRepository.save(location);
+    }
+
+    // 위치의 이름을 수정하는 메서드
+    @Transactional
+    public void updateLocation(LocationRequest locationRequest) {
+        Location findLocation = locationRepository.findByName(locationRequest.getName()).orElseThrow(
+                () -> new NotFoundException(ErrorCode.NOT_FOUND_LOCATION)
+        );
+        findLocation.updateLocation(locationRequest);
+    }
+
+    // 위치를 삭제하는 메서드
+    @Transactional
+    public void deleteLocation(LocationRequest locationRequest) {
+        Location findLocation = locationRepository.findByName(locationRequest.getName()).orElseThrow(
+                () -> new NotFoundException(ErrorCode.NOT_FOUND_LOCATION)
+        );
+        locationRepository.delete(findLocation);
     }
 }
