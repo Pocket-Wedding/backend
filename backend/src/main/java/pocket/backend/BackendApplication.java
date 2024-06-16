6b86f3dcfcd5c537526c21cc1ef254e83d15feb6
heesane <tees3359@gmail.com>
2024-06-16 21:25:10 +0900

    feat: AutoWiringSpringBeanJobFactory 빈 등록

--- backend/src/main/java/pocket/backend/BackendApplication.java
+++ backend/src/main/java/pocket/backend/BackendApplication.java
@@ -1,9 +1,12 @@
 package pocket.backend;
 
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import pocket.backend.common.config.AutoWiringSpringBeanJobFactory;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -12,6 +15,11 @@ public class BackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
+    }
+
+    @Bean
+    public AutoWiringSpringBeanJobFactory jobFactory(AutowireCapableBeanFactory beanFactory) {
+        return new AutoWiringSpringBeanJobFactory(beanFactory);
     }
 
 }
