449eb3c49ce4cff2a2f3b64ad13682dd364d37f3
heesane <tees3359@gmail.com>
2024-06-20 19:18:02 +0900

    feat: WebMvcConfigurer을 통한 CORS 문제 해결

---
+++ backend/src/main/java/pocket/backend/common/config/CorsConfig.java
@@ -0,0 +1,19 @@
+package pocket.backend.common.config;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Component
+public class CorsConfig implements WebMvcConfigurer{
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000","http://localhost:*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE","PATCH", "OPTIONS")
+                .allowedHeaders("Authorization", "Content-Type", "X-Requested-With")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
