2231dafc383dccb61c7fa688a64214e7c16bd281
heesane <tees3359@gmail.com>
2024-06-24 00:00:46 +0900

    docs: 주석 추가

--- backend/src/main/java/pocket/backend/common/config/CorsConfig.java
+++ backend/src/main/java/pocket/backend/common/config/CorsConfig.java
@@ -7,6 +7,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Component
 public class CorsConfig implements WebMvcConfigurer{
 
+
+    // 로컬에서 작업시 localhost:3000, localhost:포트번호
+    // Docker에서 작업시 container이름:포트번호
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
