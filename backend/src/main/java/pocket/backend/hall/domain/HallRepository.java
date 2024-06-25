86a225a7b306377e2d0413c33d60f7b4fb927ded
limjihoon99 <bo9701@naver.com>
2024-06-25 15:26:38 +0900

    refactor: HallRepository 수정

--- backend/src/main/java/pocket/backend/hall/domain/HallRepository.java
+++ backend/src/main/java/pocket/backend/hall/domain/HallRepository.java
@@ -3,6 +3,10 @@ package pocket.backend.hall.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface HallRepository extends JpaRepository<Hall, Long> {
+    boolean existsByName(String name);
+    Optional<Hall> findByName(String name);
 }
